9de41bc6caf166417ea497f9df1b4908f921d248
gerteck <changerteck@gmail.com>
2025-01-23 16:54:31 +0800

    Site Update. [skip ci]

---
+++ contents/choosing-ssg.page-vue-render.js
@@ -0,0 +1,73 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"sticky":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/mb-SSGs/index.html","title":"Home"}},[_c('img',{attrs:{"src":"/mb-SSGs/assets/ssgs.png","height":"70"}})])]},proxy:true},{key:"right",fn:function(){return [_c('li',[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/mb-SSGs/contents/setting-up.html"}},[_v("Setting Up")])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/mb-SSGs/contents/choosing-ssg.html"}},[_v("Choosing an SSG")])]),_v(" "),_c('dropdown',{staticClass:"nav-link",scopedSlots:_u([{key:"header",fn:function(){return [_v("Others")]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/mb-SSGs/contents/jamstack.html"}},[_v("JAMstack")])]),_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/mb-SSGs/contents/faq.html"}},[_v("FAQs")])]),_v(" "),_c('li',[_c('a',{staticClass:"dropdown-item",attrs:{"href":"/mb-SSGs/contents/lols/virus.html"}},[_v("For Fun")])])])],1)],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('overlay-source',{attrs:{"id":"site-nav","tag-name":"nav","to":"site-nav"}},[_c('div',{staticClass:"site-nav-top"},[_c('div',{staticClass:"fw-bold mb-2",staticStyle:{"font-size":"1.25rem"}},[_v("Contents")])]),_v(" "),_c('div',{staticClass:"nav-component slim-scroll"},[_c('site-nav',[_c('overlay-source',{staticClass:"site-nav-list site-nav-list-root",attrs:{"tag-name":"ul","to":"mb-site-nav"}},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/index.html"}},[_v("Home 🏠")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/markbind.html"}},[_v("MarkBind")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Vue.js \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/vuejs/nuxtjs.html"}},[_v("Nuxt.js")])])])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Ruby \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/ruby/jekyll.html"}},[_v("Jekyll")])])])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("React \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/react/gatsby.html"}},[_v("Gatsby ")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/react/nextjs.html"}},[_v("Next.js")])])])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Go \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/ruby/hugo.html"}},[_v("Hugo")])])])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Content Management Systems CMS \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/cms/wordpress.html"}},[_v("Wordpress")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/mb-SSGs/contents/cms/drupal.html"}},[_v("Drupal")])])])])])])],1)],1)]),_v(" "),_c('div',{attrs:{"id":"content-wrapper"}},[_c('breadcrumb'),_v(" "),_c('br'),_v(" "),_m(0),_v(" "),_c('p',[_v("With content managed using one or more of the techniques described in (setting up), it’s time to consider how it is transformed into production HTML.")]),_v(" "),_m(1),_v(" "),_m(2),_v(" "),_m(3),_v(" "),_c('p',[_v("Choosing a site generator can be challenging. While popularity is a factor, here are five key considerations:")]),_v(" "),_c('hr'),_v(" "),_m(4),_v(" "),_c('p',[_v("Each generator is designed with specific website types in mind:")]),_v(" "),_m(5),_v(" "),_m(6),_v(" "),_c('hr'),_v(" "),_m(7),_v(" "),_c('p',[_v("While prior programming experience isn’t always necessary, familiarity with the language can help for custom plugins and extensions.")]),_v(" "),_c('p',[_v("Examples:")]),_v(" "),_m(8),_v(" "),_c('p',[_v("Choosing a generator in a language your team knows can streamline development.")]),_v(" "),_c('hr'),_v(" "),_m(9),_v(" "),_c('p',[_v("JAMstack development involves creating HTML templates, CSS, and JavaScript. Templating languages extend HTML functionality with reusable blocks, loops, and conditional statements.")]),_v(" "),_c('p',[_v("Examples:")]),_v(" "),_m(10),_v(" "),_c('p',[_v("Choose a syntax that aligns with your team’s preferences.")]),_v(" "),_c('hr'),_v(" "),_m(11),_v(" "),_c('p',[_v("Build speed becomes critical as site content grows:")]),_v(" "),_m(12),_v(" "),_m(13),_v(" "),_c('hr'),_v(" "),_m(14),_v(" "),_c('p',[_v("Modern web development includes asset preparation, such as:")]),_v(" "),_m(15),_v(" "),_m(16),_v(" "),_m(17),_v(" "),_m(18),_v(" "),_c('hr'),_v(" "),_m(19),_v(" "),_c('p',[_v("Recommending a site generator depends on:")]),_v(" "),_m(20),_v(" "),_c('p',[_v("Static site generators evolve rapidly, making it worthwhile to experiment with several options. Always explore the documentation to understand the problems each generator solves.")])],1),_v(" "),_c('overlay-source',{attrs:{"id":"page-nav","tag-name":"nav","to":"page-nav"}},[_c('div',{staticClass:"nav-component slim-scroll"})]),_v(" "),_c('scroll-top-button')],1),_v(" "),_m(21)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{attrs:{"id":"choosing-a-site-generator"}},[_v("Choosing a Site Generator"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#choosing-a-site-generator","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"evolution-of-site-generators"}},[_v("Evolution of Site Generators"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#evolution-of-site-generators","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("In the beginning, site generators were almost synonymous with "),_c('strong',[_v("Jekyll")]),_v(", the first to gain significant traction. Created by "),_c('strong',[_v("Tom Preston-Werner")]),_v(", GitHub’s cofounder, Jekyll is written in Ruby.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Today, site generators are available in many languages, including Ruby, Python, PHP, JavaScript, and Go. The website "),_c('a',{attrs:{"href":"https://staticgen.com"}},[_v("staticgen.com")]),_v(" lists over 30 languages and showcases the popularity of different generators. Most are free and open source.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"1-the-type-of-website"}},[_v("1. The Type of Website"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#1-the-type-of-website","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("Complex taxonomy and thousands of pages")]),_v(" "),_c('li',[_v("Blogs")]),_v(" "),_c('li',[_v("Documentation")]),_v(" "),_c('li',[_v("Progressive Web Apps (PWAs) or highly interactive apps")])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('strong',[_v("Advice:")]),_v(" Explore sample projects for each generator to determine compatibility with your goals.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"2-the-programming-language-used"}},[_v("2. The Programming Language Used"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#2-the-programming-language-used","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_c('strong',[_v("Hugo")]),_v(" (Go): Simple installation; no Go knowledge required.")]),_v(" "),_c('li',[_c('strong',[_v("Jekyll")]),_v(" (Ruby): Ruby installation needed but minimal language knowledge required.")]),_v(" "),_c('li',[_c('strong',[_v("Gatsby")]),_v(" (JavaScript): Requires Node.js for installation.")])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"3-the-templating-syntax"}},[_v("3. The Templating Syntax"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#3-the-templating-syntax","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_c('strong',[_v("Jade/Pug")]),_v(": Minimalist syntax using indentation.")]),_v(" "),_c('li',[_c('strong',[_v("Handlebars/Liquid")]),_v(": Close to traditional HTML.")])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"4-speed"}},[_v("4. Speed"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#4-speed","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("Small sites: Milliseconds to build.")]),_v(" "),_c('li',[_v("Large sites: Several minutes per build.")])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('strong',[_v("Hugo")]),_v(" is known for its speed due to Go's multi-threaded architecture, whereas some generators (e.g., Gatsby, Nuxt) build JavaScript bundles alongside static HTML, increasing build times but optimizing site performance on CDNs.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"5-developer-tooling"}},[_v("5. Developer Tooling"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#5-developer-tooling","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("Image compression")]),_v(" "),_c('li',[_v("JavaScript transpilation and minification")]),_v(" "),_c('li',[_v("CSS compilation (e.g., SASS, LESS)")])])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Some generators, like "),_c('strong',[_v("Nuxt")]),_v(" and "),_c('strong',[_v("Gatsby")]),_v(", include features for asset management, often leveraging tools like "),_c('strong',[_v("Webpack")]),_v(". Others rely on external utilities.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('strong',[_v("Additional Features:")])])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_c('strong',[_v("Linting")]),_v(": Automatically tests for errors in HTML, CSS, and JavaScript.")]),_v(" "),_c('li',[_c('strong',[_v("Local Testing and Hot Reloading")]),_v(": Previews changes locally, with immediate browser updates upon saving.")])])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"final-thoughts"}},[_v("Final Thoughts"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#final-thoughts","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("Personal preference")]),_v(" "),_c('li',[_v("Familiarity")]),_v(" "),_c('li',[_v("Intended use")])])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Generated by "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v("]")])])])}
+}];
+  
